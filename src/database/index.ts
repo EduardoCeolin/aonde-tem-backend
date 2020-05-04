fd0b56da0478ca7700bff264fd71241bdc78d659
EduardoCeolin <eduardo_ceolin@hotmail.com>
2020-05-04 11:45:55 -0300

    teste 5

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,25 +1,9 @@
-import { createConnection } from "typeorm";
-import { parse } from "pg-connection-string";
+// import { createConnection } from "typeorm";
+import { Pool } from "pg";
 
-const connectionOptions = parse(
-  "postgres://kzduzaripdfyew:41dfa42666f295c4052262bd7a38b10ecfb4ac787560068e888e9a984919785d@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d70ss0d4uije0q"
-);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
-console.log(connectionOptions);
-
-const typeOrmOptions = {
-  type: "postgres",
-  host: connectionOptions.host,
-  port: connectionOptions.port,
-  username: connectionOptions.username,
-  password: connectionOptions.password,
-  database: connectionOptions.database,
-  ssl: true,
-  entities: ["./src/models/*.ts"],
-  migrations: ["./src/database/migrations/*.ts"],
-  cli: {
-    migrationsDir: "./src/database/migrations",
-  },
-};
-
-createConnection(typeOrmOptions);
+pool.connect();
+//createConnection(typeOrmOptions);
