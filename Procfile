ad30e2920706ddf4de382c4d04efc988481553bc
EduardoCeolin <eduardo_ceolin@hotmail.com>
2020-05-04 13:15:33 -0300

    teste 12

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./node_modules/typeorm/cli.js migration:run && nrimraf dist && tsc && node dist/server
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,9 @@
-// import { createConnection } from "typeorm";
-import { Client } from "pg";
+import { createConnection } from "typeorm";
+// import { Client } from "pg";
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-client.connect();
-//createConnection(typeOrmOptions);
+// client.connect();
+createConnection();
