43b06fc37025f9bc904e4e9758bbde87457ba643
EduardoCeolin <eduardo_ceolin@hotmail.com>
2020-05-03 22:37:57 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import cors from "cors";
 import routes from "./routes";
 import uploadConfig from "./config/upload";
 import AppError from "./errors/AppError";
@@ -10,6 +11,7 @@ import "./database";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.directory));
 app.use(routes);
