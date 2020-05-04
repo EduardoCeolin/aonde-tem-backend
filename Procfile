433ea49e884ffac169390541564ed9fd1cad5d6c
EduardoCeolin <eduardo_ceolin@hotmail.com>
2020-05-04 14:27:27 -0300

    herokuu

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node ./node_modules/typeorm/cli.js migration:run && nrimraf dist && tsc && node dist/server
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,10 @@
+import { Client } from "pg";
 import { createConnection } from "typeorm";
-// import { Client } from "pg";
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-// });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
 
-// client.connect();
-createConnection();
+console.log(client);
+
+client.connect();
